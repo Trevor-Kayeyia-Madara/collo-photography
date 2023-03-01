8e9cd16f523bde269f8599bfabf7a334535a6522
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-03-01 10:12:56 +0300

    Done

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const nav = document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav = () =>{
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click',openNav);
+
+const closeNav = () =>{
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+
+  closeNavBtn.addEventListener('click',closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click',closeNav);
+  })
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click',closeNav);
+  })
+
 // navbar scrolling effects
 window.addEventListener('scroll',() =>
 {
@@ -14,3 +41,14 @@ textButtons.forEach(textButton =>{
 
     text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform:rotate(${index * 12}deg)">${character}</span>`).join();
 })
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
